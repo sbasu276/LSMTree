4dbbc65adb22113772d60c6d15c527255824c9d5
tarun <cmakeshmail@gmail.com>
2018-11-16 21:55:54 -0500

    file operations working

--- file_op.py
+++ file_op.py
@@ -2,13 +2,29 @@ import sys, os
 
 PAGE_SIZE = 4096 #Bytes
 KEY_VALUE_SIZE = 16 #Bytes
+
+#Set this based on your machine
 BLOCK_SIZE_BYTES = 128 #Should be at the granularity of PAGE_SIZE
 BLOCK_SIZE_KEYS = int(BLOCK_SIZE_BYTES/KEY_VALUE_SIZE) #Should be at the granularity of PAGE_SIZE
 
 C1_FILE = 'C1.txt' #Main SSTable file name
-C0_FILE = 'C0.txt' #C0 component
 
 index_table = []
+
+class Request:
+    def __init__(self, key=None, value=None, ifDel=None, fd=None):
+        self.key = key
+        self.value = value
+        self.ifDel = ifDel #'D' if the key needs to be deleted
+
+def make_string(key, value):
+	line = key+' '+value
+	if(len(line) < 16):
+		i = 15 - len(line)
+		while(i > 0):
+			line += ' '
+			i -= 1
+	return line[:15]+'\n'
 
 def init_c1_file():
 	my_dict = {}
@@ -19,38 +35,32 @@ def init_c1_file():
 		my_dict[line[0]] = line[1]	
 
 	for key in sorted(my_dict):
-		line = key+' '+my_dict[key]
-		if(len(line) < 16):
-			i = 15 - len(line)
-			while(i > 0):
-				line += ' '
-				i -= 1
-			c1_fd.write(line+'\n')
+		line = make_string(key, my_dict[key])
+		c1_fd.write(line)
 	c1_fd.close()
 
-def init_index_table():
+def gen_index_table(fd):
 	global index_table 
 	index_table= []
 	offset = 0
-	file_size = os.path.getsize(C1_FILE)
-	c1_file = open(C1_FILE, "r+")
+	fd.seek(0, 0)
+	file_size = os.path.getsize(fd.name)
 	while (offset < file_size):
-		line = c1_file.readline()
-		items = line.strip('\n').split(' ')
+		line = fd.readline()
+		items = line.strip('\n ').split(' ')
 		index_table.append((items[0], offset))
 		offset += BLOCK_SIZE_BYTES
-		c1_file.seek(offset, 0)
+		fd.seek(offset, 0)
 
 def get_key_it(a):
 	return a[0]
 
 def get_key_sst(a):
-	return a.strip('\n').split(' ')[0]
+	return a.strip('\n ').split(' ')[0]
 
 def binarySearch(arr, l, r, x, extract_key): 
 	while l <= r: 
 		mid = int(l + (r - l)/2);
-		print(mid, l, r)
 		arr_mid = extract_key(arr[mid])
 		if arr_mid == x: 
 			return mid 
@@ -71,68 +81,87 @@ def islice(fd, offset, numLines):
 
 def get(fd, key):
 	r = binarySearch(index_table, 0, len(index_table)-1, key, get_key_it)
-	print(index_table[r])
 	search_block = islice(fd, index_table[r][1], BLOCK_SIZE_KEYS)
-	print(search_block)
 	r = binarySearch(search_block, 0, len(search_block)-1, key, get_key_sst)
 	if(get_key_sst(search_block[r]) == key):
-		print("Key found!!")
+		return search_block[r].strip('\n ').split(' ')[1]
 	else:
-		print("Key NOT found!!")
+		return None
 
-def merge():
-	print('')
-#    while i < len(L) and j < len(R):
-#        
-#        if L[i][0] < R[j][0]:
-#            arr.insert( k, L[i] )
-#            i+=1
-#            k+=1
-#        else:
-#            arr.insert( k, R[j] )
-#            j+=1
-#            k+=1
-#
-#    # Checking if any element is left
-#    while i < len(L):
-#        arr.insert( k, L[i] )
-#        i+=1
-#        k+=1
-#    while j < len(R):
-#        arr.insert( k, R[j] )
-#        j+=1
-#        k+=1
-#
-#
-#    L_fd.close()
-#    R_fd.close()
-#    return arr
-#
-##Open files
-#
-#dest_fd=open('dest.txt','w')
-#
-#
-##Merged tuple list
-#fname1='SST_1029.txt'
-#fname2='SST_1090.txt'
-#dest=merge(fname1,fname2)
-#print("DEST:",dest)
-#
-##Converting to required format for file
-#final_list=format_into_list_of_strings(dest)
-#print(final_list)
-#
-##Writing line by line to the destination file
-#dest_fd.writelines(final_list)
-#
-##Closing files
-#
-#dest_fd.close()
+def merge(c1_fd, c0_list):
+#	Generates a new merged file with the same name as c1_fd + .tmp extension\
+#	TODO: Replace the .txt file with the .tmp file
+
+	temp_file_name = c1_fd.name.strip('.txt')+'.tmp'
+	temp_file = open(temp_file_name, 'w+')
+	i = 0
+	index = 0
+	new_index_table = []
+	for line in c1_fd:
+		old_entry = Request(line.split(' ')[0], line.split(' ')[1])
+		while(i < len(c0_list)):
+			new_entry = c0_list[i]
+			if(new_entry.key > old_entry.key):
+				temp_file.write(make_string(old_entry.key, old_entry.value))
+				index += 1
+				break
+			elif(new_entry.key < old_entry.key):
+				i += 1
+				if(new_entry.ifDel != 'D'): #add to file if it is not a delete request
+					temp_file.write(make_string(new_entry.key, new_entry.value))
+					index += 1
+			else:	# both keys are equal - Dedup
+				i += 1
+				if(new_entry.ifDel == 'D'):
+					#print('Deleting '+new_entry.key)
+					break
+				else:
+					temp_file.write(make_string(new_entry.key, new_entry.value))
+					index += 1
+		if(i == len(c0_list)):
+			break
+
+	for line in c1_fd:
+		#print('Writing '+line)
+		temp_file.write(line)	
+
+	while(i < len(c0_list)):
+		new_entry = c0_list[i]
+		i += 1
+		if(new_entry.ifDel != 'D'):
+			temp_file.write(make_string(new_entry.key, new_entry.value))
+
+	temp_file.close()
 
 if __name__ == "__main__":
-	init_c1_file()
-	init_index_table()
+
+	init_c1_file() #use to init C1 file
+
 	c1_fd = open('C1.txt', 'r')
-	get(c1_fd, 'Rafael')
-	get(c1_fd, 'zzz')
+
+	gen_index_table(c1_fd) #generate index table
+
+	#testing presence of all the keys of names.txt in C1.txt
+	my_fd = open('names.txt', 'r')
+	for line in my_fd.readlines():
+		line = line.strip('\n ').split(' ')
+		value = get(c1_fd, line[0])
+		if(value):
+			#print('Key found!')
+			continue
+		else:
+			print('Key '+line[0]+' not found!')
+			exit()
+
+	#testing merging
+	c1_fd.seek(0, 0)
+	c0_list = []
+	c0_list.append(Request('Aarav', 'Ruth')) #inserting a new key at the beginning of the file
+	c0_list.append(Request('Aman', 'Jain')) #inserting a new entry in the middle
+	c0_list.append(Request('Novak', 'Dvic', 'D')) #deleting a non-existent key
+	c0_list.append(Request('Tonia', 'Jain', 'D'))	#deleting an existing entry
+	c0_list.append(Request('Warren', 'Jain', 'W')) #Adding an entry
+	c0_list.append(Request('Zzaid', 'Moph', '')) #Adding an entry at the end of file
+	merge(c1_fd, c0_list)
+
+	
