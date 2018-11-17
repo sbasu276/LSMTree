34ce8d14541c8f7b91a9778c32de585449376420
Soumen Basu <mail.basu.s@gmail.com>
2018-11-18 03:32:07 +0530

    lsm

--- src/lsm.py
+++ src/lsm.py
@@ -1,60 +1,89 @@
 import time
+from enum import Enum
 
-class C0Element:
-    def __init__(self, value, tomestone=False):
+class OP(Enum):
+    WRITE = 1
+    MERGE = 2
+
+class LsmNode:
+    def __init__(self, key, value, tomestone=False):
+        self.key = key
         self.value = value,
         self.timestamp = time.time()
         self.tombstone = tombstone
 
+class C1:
+    def get(self, key):
+        pass
+
+class WAL:
+    def __init__(self, logger):
+        pass
+
+    def log(op, args):
+        pass
+
 class LsmTree:
     def __init__(self, limit):
         self.memtable = {}
+        self.c1 = C1() #c1 class obj
         self.limit = limit
         self.size = 0
         self.buffer = None
+        self.wal = WAL() #write-ahead log
 
     def get(self, key):
-        pass
+        if key in self.memtable:
+            if self.memtable[key].tombstone:
+                return None
+            return self.memtable[key].value
+        return self.c1.get(key)
     
     def put(self, key, value):
-        self.__add(key, value)
+        if key in self.memtable:
+            node = LsmNode(key, value, tombstone=False)
+            self.memtable[key] = node
+        else:
+            self.__add(key, value)
 
     def insert(self, key, value):
-        pass
+        self.put(key, value)
 
     def delete(self, key):
-        pass
-
+        self.__add(key, value, tombstone=True)
+            
     def writeback(self, key, value):
-        pass
+        self.__add(key, value)
 
     def __is_full(self):
         return (self.size >= self.limit)
 
     def __add(self, key, value, tombstone = False)
+        node = LsmNode(key, value, tombstone=tombstone) 
+        self.memtable[key] = node
+        self.size = self.size + 1
         if self.__is_full():
             self.__flush_to_buf()
-        elem = C0Element(value, tombstone) 
-        self.memtable[key] = elem
-        self.size = self.size + 1
 
     def __merge(self):
         pass
 
     def __flush_to_buf(self):
         self.buffer = [(k, v) for k, v in self.memtable.items()]
-        self.buffer.sort(key=lambda x: int(x[0]))
         self.memtable = {}
         self.size = 0
+        self.buffer.sort(key=lambda x: int(x[0]))
+        self.__flush()
 
-    def flush(self):
+    def __flush(self):
         sst_name = "sstable_%s"%str(time.time())
+        log_args = (sst_name, self.buffer)
+        self.wal.log(OP.WRITE, log_args)
         with open(sst_name, 'w') as f:
-            for elem in self.buffer:
-                line = ",".join([elem[0], elem[1].value, \
+            for key, elem in self.buffer:
+                line = ",".join([key, elem[1].value, \
                                  elem[1].timestamp, elem[1].tombstone])
                 f.write(line+"\n")
-
 
     def show(self):
         if self.size:
